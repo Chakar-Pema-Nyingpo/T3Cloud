6385f0dc4188edfb44583592ef9ff912656cc5fc
Chakar-Pema-Nyingpo <114732678+Chakar-Pema-Nyingpo@users.noreply.github.com>
2024-02-13 13:52:00 +0600

    Added new files

---
+++ loops.js
@@ -0,0 +1,16 @@
+// var rl = require('readline-sync');
+
+// var shouldLoopRun = true
+
+// while (shouldLoopRun){
+//     var reply = rl.question("Enter a number to get the addition till 10")
+//     if (reply === "yes"){
+//         n = rl.question("Enter the persons name: ")
+//         shouldLoopRun 
+//     }else {
+//         shouldLoopRun = false
+//     }
+// }
+
+var myAnything = [4, false, "Cat", 0.423, ["Tom", 9, 41 [2,3,4]]]
+ console.log(myAnything[4][5])
